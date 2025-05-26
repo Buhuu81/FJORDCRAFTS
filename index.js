8d4a91145b090708a7250048751d7e0713e8a108
Buhuu81 <Tommyjohansen1981@gmail.com>
2025-05-26 23:18:00 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FJORDCRAFTS</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <img src="background.jpg" class="background">
+        <div class="logo">
+            <img src="logo.png" alt="Your Logo">
+        </div>
+        <div class="social-links">
+            <a href="https://facebook.com" target="_blank"><img src="facebook.png" alt="Facebook"></a>
+            <a href="https://instagram.com" target="_blank"><img src="instagram.png" alt="Instagram"></a>
+        </div>
+    </div>
+</body>
+</html>
